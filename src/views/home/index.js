7c73dac4ea4a91b85555d69641306ce8a1278d3e
kplichta <konrad.plichta@sim.com.pl>
2024-02-15 10:49:47 +0100

    create menu component

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { useTranslation } from "gatsby-plugin-react-i18next"
+import Menu from "../../components/menu/menu"
 
 const Home = () => {
   const { t } = useTranslation()
 
   return (
     <>
+      <Menu />
       <div className="container">
         <h1>{t`home.title`}</h1>
       </div>
