10c6fcada1e162d8d02d48ba6904157bef4c930f
kplichta <konrad.plichta@sim.com.pl>
2024-02-16 09:53:27 +0100

    create discover potential component

---
+++ src/views/home/components/discover-potential.js
@@ -0,0 +1,15 @@
+const DiscoverPotential = () => {
+  return (
+    <>
+      <div className="dp-container">
+        <div className="container">
+          <div>
+            <h2>{t`discover-potential.title`}</h2>
+            <p>{t`discover-potential.p`}</p>
+            <p>{t`discover-potential.p2`}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
