6c2bfa7010065911ed0fd61ba18c39a7011643b1
kplichta <konrad.plichta@sim.com.pl>
2024-02-17 12:10:00 +0100

    display speakers from contentful

--- src/views/home/components/speakers.js
+++ src/views/home/components/speakers.js
@@ -1,13 +1,70 @@
-import React from "react"
-import { useTranslation } from "gatsby-plugin-react-i18next"
+import React, { useState, useEffect, useContext } from "react"
+import { useTranslation, I18nextContext } from "gatsby-plugin-react-i18next"
 import "../styles/speakers.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Carousel from "react-bootstrap/Carousel"
 import "../../../images/profile-pic.png"
 import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
+import getCurrentTranslations from "../../../components/contentful-translator"
 
 const Speakers = () => {
   const { t } = useTranslation()
+  const { language } = useContext(I18nextContext)
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulSpeaker(sort: { createdAt: ASC }) {
+        edges {
+          node {
+            description {
+              description
+            }
+            name
+            role
+            image {
+              file {
+                url
+              }
+              title
+            }
+            node_locale
+          }
+        }
+      }
+    }
+  `)
+
+  const [speakers, setSpeakers] = useState()
+
+  useEffect(() => {
+    const getSpeakers = () => {
+      const getSpeakers = getCurrentTranslations(
+        data.allContentfulSpeaker.edges,
+        language
+      )
+      setSpeakers(getSpeakers)
+    }
+    getSpeakers()
+    console.log(speakers)
+  }, [data.allContentfulSpeaker, language])
+
+  const renderSpeakerGroups = () => {
+    const speakerGroups = []
+    for (let i = 0; i < speakers.length; i += 10) {
+      speakerGroups.push(speakers.slice(i, i + 10))
+    }
+    return speakerGroups?.map((group, index) => (
+      <Carousel.Item key={index} interval={60000}>
+        <div className="speakers-container">
+          {group.map((speaker, idx) => (
+            <div className="speaker" key={idx}>
+              <h1>{speaker.node.name}</h1>
+            </div>
+          ))}
+        </div>
+      </Carousel.Item>
+    ))
+  }
 
   return (
     <>
@@ -17,108 +74,7 @@ const Speakers = () => {
       <div className="s-container">
         <div className="container">
           <h2>{t`speakers.title`}</h2>
-          <Carousel>
-            <Carousel.Item interval={60000}>
-              <div className="speakers-container">
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-                <div className="speaker">
-                  <StaticImage
-                    className="o-image"
-                    src="../../../images/organizers/o-beta-etf.png"
-                    alt="Beta ETF"
-                    placeholder="none"
-                    loading="lazy"
-                  />
-                </div>
-              </div>
-            </Carousel.Item>
-            <Carousel.Item interval={60000}>
-              <div className="speakers-container"></div>
-            </Carousel.Item>
-            <Carousel.Item interval={60000}>
-              <div className="speakers-container"></div>
-            </Carousel.Item>
-          </Carousel>
+          <Carousel>{renderSpeakerGroups()}</Carousel>
         </div>
       </div>
     </>
