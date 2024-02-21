56db11c2b373f0ed47ae6ee6804c4baa79265b65
kplichta <konrad.plichta@sim.com.pl>
2024-02-21 11:46:34 +0100

    agenda styles

--- src/views/home/components/agenda.js
+++ src/views/home/components/agenda.js
@@ -3,21 +3,22 @@ import { useTranslation, I18nextContext } from "gatsby-plugin-react-i18next"
 import "../styles/agenda.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Carousel from "react-bootstrap/Carousel"
+import { StaticImage } from "gatsby-plugin-image"
 
 const Agenda = () => {
   const { t } = useTranslation()
   const { language } = useContext(I18nextContext)
 
   const dayOneButton = (
-    <div>
-      <h4>{t`agenda.title-day1`}</h4>
-      <p>{t`agenda.description-day1`}</p>
+    <div className="agenda-day-chooser">
+      <h4 className="h4-style">{t`agenda.title-day1`}</h4>
+      <p className="p-style">{t`agenda.description-day1`}</p>
     </div>
   )
   const dayTwoButton = (
-    <div>
-      <h4>{t`agenda.title-day2`}</h4>
-      <p>{t`agenda.description-day2`}</p>
+    <div className="agenda-day-chooser">
+      <h4 className="h4-style">{t`agenda.title-day2`}</h4>
+      <p className="p-style">{t`agenda.description-day2`}</p>
     </div>
   )
 
@@ -27,7 +28,96 @@ const Agenda = () => {
         <div className="container">
           <h2 className="h2-style">{t`agenda.title`}</h2>
           <h4 className="h4-style">{t`agenda.title2`}</h4>
-          <Carousel nextIcon={dayOneButton} prevIcon={dayTwoButton}></Carousel>
+          {/* <Carousel> */}
+          <Carousel
+            prevIcon={dayOneButton}
+            nextIcon={dayTwoButton}
+            interval={null}
+            indicators={false}
+          >
+            {/* <div>
+              <a
+                class="carousel-control-prev"
+                role="button"
+                tabindex="0"
+                href="#"
+              >
+                <div class="agenda-day-chooser">
+                  <h4 class="h4-style">agenda.title-day2</h4>
+                  <p>agenda.description-day2</p>
+                </div>
+                <span class="visually-hidden">Previous</span>
+              </a>
+            </div> */}
+            <Carousel.Item>
+              <div className="event-agenda-container">
+                <div className="time-con">
+                  <p>08:00 - 10:00</p>
+                  <p>2h 0m</p>
+                </div>
+                <div className="right-con">
+                  <div className="description-con">
+                    <h5>
+                      The Art of Public Speaking and Effective Communication
+                    </h5>
+                    <p>
+                      The program provides an overview of the core GRC modules
+                      in ServiceNow - Policy and Compliance, Risk, This course
+                      consists of hands-on labs that provide a holistic
+                      experience of optimally configuring SA
+                    </p>
+                  </div>
+                  <div className="speaker-con">
+                    <div className="speaker-desc-con">
+                      <p>Albert Flores</p>
+                      <p>General Electric</p>
+                    </div>
+                    <StaticImage
+                      className="o-image"
+                      src="../../../images/organizers/profile-pic.png"
+                      alt="Beta ETF"
+                      placeholder="none"
+                      loading="lazy"
+                    />
+                  </div>
+                </div>
+              </div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div className="event-agenda-container">
+                <div className="time-con">
+                  <p>08:00 - 10:00</p>
+                  <p>2h 0m</p>
+                </div>
+                <div className="right-con">
+                  <div className="description-con">
+                    <h5>
+                      The Art of Public Speaking and Effective Communication
+                    </h5>
+                    <p>
+                      The program provides an overview of the core GRC modules
+                      in ServiceNow - Policy and Compliance, Risk, This course
+                      consists of hands-on labs that provide a holistic
+                      experience of optimally configuring SA
+                    </p>
+                  </div>
+                  <div className="speaker-con">
+                    <div className="speaker-desc-con">
+                      <p>Albert Flores</p>
+                      <p>General Electric</p>
+                    </div>
+                    <StaticImage
+                      className="o-image"
+                      src="../../../images/organizers/profile-pic.png"
+                      alt="Beta ETF"
+                      placeholder="none"
+                      loading="lazy"
+                    />
+                  </div>
+                </div>
+              </div>
+            </Carousel.Item>
+          </Carousel>
         </div>
       </div>
     </>
