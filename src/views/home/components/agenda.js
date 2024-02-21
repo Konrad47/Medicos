17494f31de722c4f7a5c451d895faa3a2a2c2559
kplichta <konrad.plichta@sim.com.pl>
2024-02-21 15:09:28 +0100

    update agenda

--- src/views/home/components/agenda.js
+++ src/views/home/components/agenda.js
@@ -3,14 +3,60 @@ import { useTranslation, I18nextContext } from "gatsby-plugin-react-i18next"
 import "../styles/agenda.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Carousel from "react-bootstrap/Carousel"
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
+import getCurrentTranslations from "../../../components/contentful-translator"
 
 const Agenda = () => {
   const { t } = useTranslation()
   const { language } = useContext(I18nextContext)
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulAgenda(sort: { createdAt: ASC }) {
+        edges {
+          node {
+            day
+            description
+            title
+            panelDuration
+            panelTime
+            break
+            speakerRole
+            speakerName
+            speakerImage {
+              gatsbyImageData(
+                width: 90
+                height: 90
+                cornerRadius: 90
+                quality: 100
+              )
+            }
+            node_locale
+          }
+        }
+      }
+    }
+  `)
+
+  const [day1, setDay1] = useState()
+  const [day2, setDay2] = useState()
+
+  useEffect(() => {
+    const getDays = () => {
+      const getDays = getCurrentTranslations(
+        data.allContentfulAgenda.edges,
+        language
+      )
+      const getDay1 = getDays.filter(day => day.node.day === true)
+      const getDay2 = getDays.filter(day => day.node.day === false)
+      setDay1(getDay1)
+      setDay2(getDay2)
+    }
+    getDays()
+  }, [data.allContentfulAgenda, language])
 
   const dayOneButton = (
-    <div className="agenda-day-chooser">
+    <div onClick={() => disableButton()} className="agenda-day-chooser">
       <h4 className="h4-style">{t`agenda.title-day1`}</h4>
       <p className="p-style">{t`agenda.description-day1`}</p>
     </div>
@@ -21,6 +67,70 @@ const Agenda = () => {
       <p className="p-style">{t`agenda.description-day2`}</p>
     </div>
   )
+
+  const disableButton = () => {
+    const carouselControlPrev = document.querySelector(
+      ".a-container .carousel-control-prev"
+    )
+    carouselControlPrev.style.pointerEvents = "none"
+    console.log(carouselControlPrev.style)
+  }
+
+  const renderDay1 = day1?.map(agenda => (
+    <div className="event-agenda-container">
+      <div className="time-con">
+        <p className="p-time">{agenda.node.panelTime}</p>
+        <p className="p-duration">{agenda.node.panelDuration}</p>
+      </div>
+      <div className="right-con">
+        <div className="description-con">
+          <h5 className="h5-style">{agenda.node.title}</h5>
+          <p className="p-style">{agenda.node.description}</p>
+        </div>
+        <div className="speaker-con">
+          <div className="speaker-desc-con">
+            <p className="p-speaker-name">{agenda.node.speakerName}</p>
+            <p className="p-speaker-desc">{agenda.node.speakerRole}</p>
+          </div>
+          <div className="a-image-container">
+            <GatsbyImage
+              className="a-speaker-image"
+              image={getImage(agenda.node?.speakerImage?.gatsbyImageData)}
+            />
+            <div className="a-speaker-image-border"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ))
+
+  const renderDay2 = day2?.map(agenda => (
+    <div className="event-agenda-container">
+      <div className="time-con">
+        <p className="p-time">{agenda.node.panelTime}</p>
+        <p className="p-duration">{agenda.node.panelDuration}</p>
+      </div>
+      <div className="right-con">
+        <div className="description-con">
+          <h5 className="h5-style">{agenda.node.title}</h5>
+          <p className="p-style">{agenda.node.description}</p>
+        </div>
+        <div className="speaker-con">
+          <div className="speaker-desc-con">
+            <p className="p-speaker-name">{agenda.node.speakerName}</p>
+            <p className="p-speaker-desc">{agenda.node.speakerRole}</p>
+          </div>
+          <div className="a-image-container">
+            <GatsbyImage
+              className="a-speaker-image"
+              image={getImage(agenda.node?.speakerImage?.gatsbyImageData)}
+            />
+            <div className="a-speaker-image-border"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  ))
 
   return (
     <>
@@ -33,78 +143,10 @@ const Agenda = () => {
             nextIcon={dayTwoButton}
             interval={null}
             indicators={false}
+            // wrap={false}
           >
-            <Carousel.Item>
-              <div className="event-agenda-container">
-                <div className="time-con">
-                  <p>08:00 - 10:00</p>
-                  <p>2h 0m</p>
-                </div>
-                <div className="right-con">
-                  <div className="description-con">
-                    <h5>
-                      The Art of Public Speaking and Effective Communication
-                    </h5>
-                    <p>
-                      The program provides an overview of the core GRC modules
-                      in ServiceNow - Policy and Compliance, Risk, This course
-                      consists of hands-on labs that provide a holistic
-                      experience of optimally configuring SA
-                    </p>
-                  </div>
-                  <div className="speaker-con">
-                    <div className="speaker-desc-con">
-                      <p>Albert Flores</p>
-                      <p>General Electric</p>
-                    </div>
-                    <StaticImage
-                      className="o-image"
-                      src="../../../images/organizers/profile-pic.png"
-                      alt="Beta ETF"
-                      placeholder="none"
-                      loading="lazy"
-                    />
-                  </div>
-                </div>
-              </div>
-            </Carousel.Item>
-            <Carousel.Item>
-              <div className="event-agenda-container">
-                <div className="time-con">
-                  <p className="p-time">08:00 - 10:00</p>
-                  <p className="p-duration">2h 0m</p>
-                </div>
-                <div className="right-con">
-                  <div className="description-con">
-                    <h5 className="h5-style">
-                      The Art of Public Speaking and Effective Communication
-                    </h5>
-                    <p className="p-style">
-                      The program provides an overview of the core GRC modules
-                      in ServiceNow - Policy and Compliance, Risk, This course
-                      consists of hands-on labs that provide a holistic
-                      experience of optimally configuring SA
-                    </p>
-                  </div>
-                  <div className="speaker-con">
-                    <div className="speaker-desc-con">
-                      <p className="p-speaker-name">Albert Flores</p>
-                      <p className="p-speaker-desc">General Electric</p>
-                    </div>
-                    <div className="a-image-container">
-                      <StaticImage
-                        className="a-speaker-image"
-                        src="../../../images/profile-pic.png"
-                        alt="Beta ETF"
-                        placeholder="none"
-                        loading="lazy"
-                      />
-                      <div className="a-speaker-image-border"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Carousel.Item>
+            <Carousel.Item>{day1 && renderDay1}</Carousel.Item>
+            <Carousel.Item>{day2 && renderDay2}</Carousel.Item>
           </Carousel>
         </div>
       </div>
