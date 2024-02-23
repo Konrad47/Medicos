aaf15ed83377527134c90b40f55ef0f33689dae6
kplichta <konrad.plichta@sim.com.pl>
2024-02-23 14:34:35 +0100

    update partners

--- src/views/home/components/partners.js
+++ src/views/home/components/partners.js
@@ -1,10 +1,86 @@
-import React, { useState, useEffect } from "react"
-import { useTranslation } from "gatsby-plugin-react-i18next"
+import React, { useState, useEffect, useContext } from "react"
+import { useTranslation, I18nextContext } from "gatsby-plugin-react-i18next"
 import "../styles/partners.css"
 import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
+import { graphql, useStaticQuery } from "gatsby"
+import getCurrentTranslations from "../../../components/contentful-translator"
 
 const Partners = () => {
   const { t } = useTranslation()
+  const { language } = useContext(I18nextContext)
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulGpwPartner {
+        edges {
+          node {
+            title
+            category
+            image {
+              gatsbyImageData(width: 180, height: 100, quality: 100)
+            }
+            node_locale
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
+
+  const [mainPartners, setMainPartners] = useState()
+  const [supportPartners, setSupportPartners] = useState()
+  const [contentPartners, setContentPartners] = useState()
+  const [partners, setPartners] = useState()
+  const [mediaPartners, setMediaPartners] = useState()
+
+  useEffect(() => {
+    const getPartners = () => {
+      const getPartners = getCurrentTranslations(
+        data.allContentfulGpwPartner.edges,
+        language
+      )
+
+      setMainPartners(
+        getPartners.filter(partner => partner.node.category === "main-partner")
+      )
+      setSupportPartners(
+        getPartners.filter(
+          partner => partner.node.category === "support-partner"
+        )
+      )
+
+      setContentPartners(
+        getPartners.filter(
+          partner => partner.node.category === "content-partner"
+        )
+      )
+
+      setPartners(
+        getPartners.filter(partner => partner.node.category === "partner")
+      )
+
+      setMediaPartners(
+        getPartners.filter(partner => partner.node.category === "media-partner")
+      )
+    }
+    getPartners()
+
+    console.log(mainPartners)
+    console.log(supportPartners)
+    console.log(contentPartners)
+    console.log(partners)
+    console.log(mediaPartners)
+  }, [data.allContentfulGpwPartner])
+
+  const renderPartners = items => {
+    return items?.map(item => (
+      <GatsbyImage
+        alt={item.node.title}
+        placeholder={item.node.title}
+        className="partner-image"
+        image={getImage(item.node.image.gatsbyImageData)}
+      />
+    ))
+  }
 
   return (
     <>
@@ -18,264 +94,66 @@ const Partners = () => {
         />
         <div className="container">
           <h2 className="h2-style">{t`partners.title`}</h2>
-          <div className="main-partners-con">
-            <h3 className="h3-style">{t`partners.main-partners`}</h3>
-            <div className="main-partners">
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-            </div>
-          </div>
-          <div className="support-content-partners-con">
-            <div className="support-partners-con">
-              <h3 className="h3-style">{t`partners.support-partners`}</h3>
-              <div className="support-partners">
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
+          {mainPartners && mainPartners.length > 0 && (
+            <div className="main-partners-con">
+              <h3 className="h3-style">{t`partners.main-partners`}</h3>
+              <div className="main-partners">
+                {renderPartners(mainPartners)}
               </div>
             </div>
-            <div className="content-partners-con">
-              <h3 className="h3-style">{t`partners.content-partners`}</h3>
-              <div className="content-partners">
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
-                <StaticImage
-                  className="partner-image"
-                  src="../../../images/organizers/o-m-bank.png"
-                  alt="Bottom image"
-                  placeholder="Bottom image"
-                  loading="lazy"
-                />
+          )}
+          {supportPartners &&
+          supportPartners.length > 0 &&
+          contentPartners &&
+          contentPartners.length > 0 ? (
+            <div className="support-content-partners-con">
+              <div className="support-partners-con">
+                <h3 className="h3-style">{t`partners.support-partners`}</h3>
+                <div className="support-partners">
+                  {renderPartners(supportPartners)}
+                </div>
+              </div>
+              <div className="content-partners-con">
+                <h3 className="h3-style">{t`partners.content-partners`}</h3>
+                <div className="content-partners">
+                  {renderPartners(contentPartners)}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="partners-con">
-            <h3 className="h3-style">{t`partners.partners`}</h3>
-            <div className="partners">
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
+          ) : (
+            <>
+              {supportPartners && supportPartners.length > 0 && (
+                <div className="support-partners-con margin48">
+                  <h3 className="h3-style">{t`partners.support-partners`}</h3>
+                  <div className="support-partners">
+                    {renderPartners(supportPartners)}
+                  </div>
+                </div>
+              )}
+              {contentPartners && contentPartners.length > 0 && (
+                <div className="content-partners-con margin48">
+                  <h3 className="h3-style">{t`partners.content-partners`}</h3>
+                  <div className="content-partners">
+                    {renderPartners(contentPartners)}
+                  </div>
+                </div>
+              )}
+            </>
+          )}
+          {partners && partners.length > 0 && (
+            <div className="partners-con">
+              <h3 className="h3-style">{t`partners.partners`}</h3>
+              <div className="partners">{renderPartners(partners)}</div>
             </div>
-          </div>
-          <div className="media-partners-con">
-            <h3 className="h3-style">{t`partners.media-partners`}</h3>
-            <div className="media-partners">
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
+          )}
+          {mediaPartners && mediaPartners.length > 0 && (
+            <div className="media-partners-con">
+              <h3 className="h3-style">{t`partners.media-partners`}</h3>
+              <div className="media-partners">
+                {mediaPartners && renderPartners(mediaPartners)}
+              </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </>
