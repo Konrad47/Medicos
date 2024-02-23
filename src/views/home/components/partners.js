9b6f9bfc22b7b247999275fb84b0b4ba8b9d2b06
kplichta <konrad.plichta@sim.com.pl>
2024-02-23 13:38:18 +0100

    update partners style

--- src/views/home/components/partners.js
+++ src/views/home/components/partners.js
@@ -17,6 +17,7 @@ const Partners = () => {
           loading="lazy"
         />
         <div className="container">
+          <h2 className="h2-style">{t`partners.title`}</h2>
           <div className="main-partners-con">
             <h3 className="h3-style">{t`partners.main-partners`}</h3>
             <div className="main-partners">
@@ -27,28 +28,123 @@ const Partners = () => {
                 placeholder="Bottom image"
                 loading="lazy"
               />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
             </div>
           </div>
           <div className="support-content-partners-con">
-            <div className="support-partners">
+            <div className="support-partners-con">
               <h3 className="h3-style">{t`partners.support-partners`}</h3>
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
+              <div className="support-partners">
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+              </div>
             </div>
-            <div className="content-partners">
+            <div className="content-partners-con">
               <h3 className="h3-style">{t`partners.content-partners`}</h3>
-              <StaticImage
-                className="partner-image"
-                src="../../../images/organizers/o-m-bank.png"
-                alt="Bottom image"
-                placeholder="Bottom image"
-                loading="lazy"
-              />
+              <div className="content-partners">
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+                <StaticImage
+                  className="partner-image"
+                  src="../../../images/organizers/o-m-bank.png"
+                  alt="Bottom image"
+                  placeholder="Bottom image"
+                  loading="lazy"
+                />
+              </div>
             </div>
           </div>
           <div className="partners-con">
@@ -61,11 +157,116 @@ const Partners = () => {
                 placeholder="Bottom image"
                 loading="lazy"
               />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
             </div>
           </div>
-          <div className="main-partners-con">
+          <div className="media-partners-con">
             <h3 className="h3-style">{t`partners.media-partners`}</h3>
             <div className="media-partners">
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
+              <StaticImage
+                className="partner-image"
+                src="../../../images/organizers/o-m-bank.png"
+                alt="Bottom image"
+                placeholder="Bottom image"
+                loading="lazy"
+              />
               <StaticImage
                 className="partner-image"
                 src="../../../images/organizers/o-m-bank.png"
