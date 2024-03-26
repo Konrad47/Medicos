721bea63c6bcbdaf5565b9cd8a930ed756f0f0d7
kplichta <konrad.plichta@sim.com.pl>
2024-03-26 14:52:18 +0100

    filter materials

--- src/views/materials/index.js
+++ src/views/materials/index.js
@@ -1,16 +1,93 @@
-import React, { useState } from "react"
+import React, { useEffect, useState, useContext } from "react"
 import Seo from "../../components/seo"
-import { useTranslation } from "gatsby-plugin-react-i18next"
+import { useTranslation, I18nextContext } from "gatsby-plugin-react-i18next"
 import MaterialsHeader from "./components/materialsHeader"
 import Layout from "../../components/layout"
 import MaterialsFilter from "./components/materialsFilter"
+import { graphql, useStaticQuery } from "gatsby"
+import getCurrentTranslations from "../../components/contentful-translator"
 
 const Materials = () => {
   const { t } = useTranslation()
-
+  const { language } = useContext(I18nextContext)
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulMaterials {
+        edges {
+          node {
+            category
+            color
+            node_locale
+            pH
+            slug
+            title
+            inic
+            cas
+            form
+            generalInformation {
+              raw
+            }
+            application {
+              raw
+            }
+          }
+        }
+      }
+    }
+  `)
   const [searchMaterial, setSearchMaterial] = useState("")
   const [selectedIndustry, setSelectedIndustry] = useState("")
   const [selectedSort, setSelectedSort] = useState("name-up")
+  const [searchedData, setSearchedData] = useState([])
+
+  useEffect(() => {
+    const getData = () => {
+      const materials = getCurrentTranslations(
+        data.allContentfulMaterials.edges,
+        language
+      )
+
+      let filteredMaterials = materials.filter(material => {
+        const titleMatches = material.node.title
+          .toLowerCase()
+          .includes(searchMaterial.toLowerCase())
+        const inicMatches = material.node.inic
+          .toLowerCase()
+          .includes(searchMaterial.toLowerCase())
+        const casMatches = material.node.cas
+          .toLowerCase()
+          .includes(searchMaterial.toLowerCase())
+        const categoryMatches = material.node.category.some(category =>
+          category.toLowerCase().includes(selectedIndustry.toLowerCase())
+        )
+
+        return (titleMatches || inicMatches || casMatches) && categoryMatches
+      })
+
+      const sortedMaterials = filteredMaterials.sort((a, b) => {
+        if (selectedSort === "name-up") {
+          return a.node.title.localeCompare(b.node.title)
+        } else if (selectedSort === "name-down") {
+          return b.node.title.localeCompare(a.node.title)
+        } else if (selectedSort === "inci-up") {
+          return a.node.inic.localeCompare(b.node.inic)
+        } else if (selectedSort === "inci-down") {
+          return b.node.inic.localeCompare(a.node.inic)
+        }
+      })
+
+      setSearchedData(sortedMaterials)
+      console.log(sortedMaterials)
+    }
+
+    getData()
+  }, [
+    data.allContentfulMaterials,
+    language,
+    searchMaterial,
+    selectedIndustry,
+    selectedSort,
+  ])
 
   const handleSearchMaterialChange = value => {
     setSearchMaterial(value)
