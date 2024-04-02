23cc847d37a1cfd8aabbbb905965aee52fb2c9e8
kplichta <konrad.plichta@sim.com.pl>
2024-04-02 08:12:25 +0200

    update services page

--- src/views/services/index.js
+++ src/views/services/index.js
@@ -1,18 +1,19 @@
 import React from "react"
 import Seo from "../../components/seo"
 import { useTranslation } from "gatsby-plugin-react-i18next"
+import Layout from "../../components/layout"
 
 const Services = () => {
   const { t } = useTranslation()
 
   return (
-    <>
+    <Layout>
       <Seo
         title={t`seo.services.title`}
         description={t`seo.services.description`}
       />
       <h1>Services Page</h1>
-    </>
+    </Layout>
   )
 }
 export default Services
