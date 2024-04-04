2712cade64d1f1fdda5e89dac705e3621271dd86
kplichta <konrad.plichta@sim.com.pl>
2024-04-04 15:43:30 +0200

    update material content and pagination

--- src/views/materials/components/materialsContent.js
+++ src/views/materials/components/materialsContent.js
@@ -21,19 +21,45 @@ const MaterialsContent = ({ materialsContent, resetFilters }) => {
     return content.map((con, index) => (
       <div className="result" key={index}>
         <h4 className="h4-style">{con.node.title}</h4>
+        <div className="up-text">
+          <p className="p-style">{t`materials-content.inci`}</p>
+          <span className="p-style">{con.node.inci}</span>
+        </div>
+        <div className="down-text">
+          <p className="p-style">{t`materials-content.cas`}</p>
+          <span className="p-style">{con.node.cas}</span>
+        </div>
         <div className="categories">
           {con.node.category.map((cat, index) => (
-            <div className="category" key={index}>
-              <p className="p-style">{cat}</p>
+            <div key={index}>
+              {cat === "Chemia gospodarcza" && (
+                <div className="category hc-background">
+                  <p className="p-style ">{t`materials-filter.household-chemicals`}</p>
+                </div>
+              )}
+              {cat === "Kosmetyka" && (
+                <div className="category cos-background">
+                  <p className="p-style ">{t`materials-filter.cosmetology`}</p>
+                </div>
+              )}
+              {cat === "Farmacja" && (
+                <div className="category far-background">
+                  <p className="p-style ">{t`materials-filter.pharmacy`}</p>
+                </div>
+              )}
+              {cat === "Żywność i suplementy diety" && (
+                <div className="category food-background">
+                  <p className="p-style ">{t`materials-filter.food`}</p>
+                </div>
+              )}
+              {cat === "Pozostałe branże" && (
+                <div className="category oi-background">
+                  <p className="p-style ">{t`materials-filter.other-industries`}</p>
+                </div>
+              )}
             </div>
           ))}
         </div>
-        <p className="p-style">
-          {t`materials-content.inci`} <span>{con.node.inci}</span>
-        </p>
-        <p className="p-style">
-          {t`materials-content.cas`} <span>{con.node.cas}</span>
-        </p>
         <p className="p-style details">{t`materials-content.details`}</p>
       </div>
     ))
