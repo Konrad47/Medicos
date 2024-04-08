6bf80ff795cb2dbab8dc8d172fd675a85a606d3a
kplichta <konrad.plichta@sim.com.pl>
2024-04-08 11:05:55 +0200

    create news header component

--- src/views/news/index.js
+++ src/views/news/index.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Seo from "../../components/seo"
 import { useTranslation } from "gatsby-plugin-react-i18next"
+import Layout from "../../components/layout"
+import NewsHeader from "./components/newsHeader"
 
 const News = () => {
   const { t } = useTranslation()
 
   return (
-    <>
+    <Layout>
       <Seo title={t`seo.news.title`} description={t`seo.news.description`} />
-      <h1>News Page</h1>
-    </>
+      <NewsHeader />
+    </Layout>
   )
 }
 export default News
