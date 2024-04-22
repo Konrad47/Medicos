9aeeb3fc821433dc87b66d389ff1ed7642998c92
kplichta <konrad.plichta@sim.com.pl>
2024-04-22 08:52:36 +0200

    add arrow to button

--- src/components/materialComponent/components/materialDiscover.js
+++ src/components/materialComponent/components/materialDiscover.js
@@ -68,10 +68,45 @@ const MaterialDiscover = ({
               )}
             </>
           )}
-          <button
-            onClick={goToMaterials}
-            className="bright-button"
-          >{t`materials.more-materials`}</button>
+          <button onClick={goToMaterials} className="bright-button">
+            {t`materials.more-materials`}{" "}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <g clip-path="url(#clip0_889_6898)">
+                <path
+                  d="M19 12L5 12"
+                  stroke="#4D8CE5"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M19 12L13 6"
+                  stroke="#4D8CE5"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M19 12L13 18"
+                  stroke="#4D8CE5"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+              <defs>
+                <clipPath id="clip0_889_6898">
+                  <rect width="24" height="24" fill="white" />
+                </clipPath>
+              </defs>
+            </svg>
+          </button>
         </div>
       </div>
     </>
