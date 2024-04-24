a11f2c70d8578ae9dadc042cb80d2ad192351a70
kplichta <konrad.plichta@sim.com.pl>
2024-04-24 19:02:16 +0200

    update

--- src/utils/templateRenderOption.js
+++ src/utils/templateRenderOption.js
@@ -14,7 +14,6 @@ export const richTextRenderOptions = {
       )
     },
     [BLOCKS.PARAGRAPH]: (node, children) => {
-      // Function to render a single paragraph
       const renderParagraph = content => {
         return (
           <p>
