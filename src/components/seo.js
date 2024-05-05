d9703a64a7572c3454093c130f2fcea92fcb0d2b
kplichta <konrad.plichta@sim.com.pl>
2024-05-05 20:31:33 +0200

    update seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -45,7 +45,37 @@ function Seo({ description, meta = [], title, ogImage }) {
           content: metaOgImage,
         },
       ].concat(meta)}
-    ></Helmet>
+    >
+      <script type="application/ld+json">
+        {`
+      {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "url": "https://medicos.com.pl/",
+        "name": "Medicos Sp. z o.o.",
+        "isAccessibleForFree": true,
+        "description": "Nasza firma oferuje surowce chemiczne i składniki aktywne z doradztwem i zabezpieczoną dostawą dla innowacyjnych rozwiązań.",
+        "email": "kontakt@medicos.com.pl",
+        "contactPoint": {
+          "@type": "ContactPoint",
+          "name": "Informacja",
+          "telephone": "+48 601 939 903",
+          "availableLanguage": "English, Polish",
+        }
+        "address": {
+          "@type": "PostalAddress",
+          "name": "ul. Ukryty Raj 4 lok. 1 02-757 Warszawa",
+          "addressCountry": "Polska",
+          "addressLocality": "Warszawa",
+          "addressRegion": "Mazowieckie",
+          "postalCode": "02-757",
+          "streetAddress": "ul. Ukryty Raj 4",
+          "@id": "https://maps.app.goo.gl/5YL6MD26Gk3tBaiA7"
+        },
+      }
+    `}
+      </script>
+    </Helmet>
   )
 }
 
